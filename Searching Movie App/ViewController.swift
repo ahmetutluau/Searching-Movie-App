7e93e04a166c8f89435b1a87a365d8ad7eb2c315
Ahmet Utlu <ahmet.utlu@id3.com.tr>
2022-08-19 15:16:03 -0700

    Initial Commit

---
+++ Searching Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Searching Movie App
+//
+//  Created by MAC on 19.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
